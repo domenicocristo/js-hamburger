cb9a88d863f952f149f42c04383bd25fe2875892
MyUsername <my@email.com>
2021-10-22 17:09:03 +0200

    commit

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-const  hamburger = document.querySelector(".fa-bars");
-hamburger.addEventListener(`hamburger`,
+const  hamburger = document.querySelector(".hamburger-menu");
+const close = document.querySelector(".close");
+const icon = document.querySelector(".fa-bars");
+// hamburger.addEventListener(`click`,
 
-function() {
-    
+function toggleMenu() {
+    if (hamburger.classList.contains("showMenu")) {
+        hamburger.classList.remove("showMenu");
+        close.style.display = "none"; 
+        icon.style.display = "block";
+    } else {
+        hamburger.classList.remove("showMenu");
+        close.style.display = "block"; 
+        icon.style.display = "none";
+    }
 }
-);
+// );
+
+hamburger.addEventListener("click", toggleMenu);
